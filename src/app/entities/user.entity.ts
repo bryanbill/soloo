f362522f6d0d377ae183d14fefae9b1fae9e034f
bryanbill <emobrya@gmail.com>
2021-11-07 11:53:32 +0300

    scaffolding

--- src/app/entities/user.entity.ts
+++ src/app/entities/user.entity.ts
@@ -1,5 +1,4 @@
-// import { hashPassword } from '@foal/core';
-import { BaseEntity, /*Column, */Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
@@ -7,14 +6,22 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // @Column({ unique: true })
-  // email: string;
+  @Column({ unique: true })
+  email: string;
 
-  // @Column()
-  // password: string;
+  @Column()
+  phone: string;
 
-  // async setPassword(password: string) {
-  //   this.password = await hashPassword(password);
-  // }
+  @Column()
+  name: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  username: string;
 
 }
+
+// This line is required. It will be used to create the SQL session table later in the tutorial.
+export { DatabaseSession } from '@foal/typeorm';
