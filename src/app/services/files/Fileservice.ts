e0f608ee6ea8c36fc265aaf20003a67f1e344e7b
TrekHub <teddyolweny@gmail.com>
2021-11-10 12:43:26 +0300

    File Service: create, read and delete files

---
+++ src/app/services/files/Fileservice.ts
@@ -0,0 +1,42 @@
+import { Readable } from "stream";
+
+import { dependency } from "@foal/core";
+import { Disk } from "@foal/storage";
+
+class FileService {
+  @dependency
+  disk: Disk;
+  /**
+   * Reads the file at the given path.
+   * @param path
+   * @returns file
+   */
+  async readFile(path: string) {
+    const file = this.disk.read(path, "buffer");
+    return file;
+  }
+  /**
+   * Creates a new file from the given content
+   * @param content
+   * @returns
+   */
+  async createFile(content: Buffer | Readable) {
+    const { path } = await this.disk.write("avatars", content);
+    // path === 'avatars/zMurtEGl1BTNJnJjeOfwx4GPWirZpoGRG9J8NbRRkRY=.jpg'
+    return path;
+  }
+
+  /**
+   * Delete the file at the given path.
+   * @param path
+   * @returns
+   */
+  async deleteFile(path: string) {
+    const stat = this.disk
+      .delete(path)
+      .then(() => true)
+      .catch(() => false);
+
+    return await stat;
+  }
+}
